8ab747ce902b229867f9612045c20961cbbf2365
SRVignesh2000 <vignesh.ramachandran@fwsa.freshworks.com>
2023-07-10 17:17:21 +0530

    day01 completed

---
+++ src/main/java/day01/solved/MyClass1.java
@@ -0,0 +1,17 @@
+package day01.solved;
+
+public class MyClass1 {
+	
+	 // Declare a static variable
+    public static int staticVariable;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Access the static variable using the class name
+        MyClass1.staticVariable = 10;
+        System.out.println(MyClass1.staticVariable);
+
+	}
+
+}
